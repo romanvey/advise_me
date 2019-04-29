32ca87fb32842a6f565ae4a8431616cf3f10ea1d
Roman Vey <roman.vey@gmail.com>
2019-04-30 01:45:38 +0300

    added commands handler and better display

--- adviseMe/src/app/components/console/console.component.ts
+++ adviseMe/src/app/components/console/console.component.ts
@@ -10,7 +10,8 @@ import { RestService } from '../../services/rest.service';
 })
 export class ConsoleComponent implements OnInit {
 
-  
+  mainCommands = ['lose weight', 'give up smoking', 'gain muscle'];
+  additionalCommands = ['weather', 'open', 'help'];
   recentMessages = [];
   messageContent = '';
   @ViewChild('messageContainer') messageContainer: ElementRef;
@@ -20,29 +21,101 @@ export class ConsoleComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.restService.getWeatherData('Lviv').subscribe(data => {
-        console.log(data);
-      }
-    );
+    this.getHelp();
   }
 
-  sendMessage() {
-    if (this.messageContent !== '') {
-      this.recentMessages.push(new MessageComponent(this.messageContent, true));
-      this.messageContent = '';
-      setTimeout(() => {this.scrollDownMessageContainer()}, 100);
+
+  sendYourMessage() {
+    let lastCommand = this.messageContent;
+    this.messageContent = '';
+    if (lastCommand !== '') {
+      this.recentMessages.push(new MessageComponent(lastCommand.split('\n'), true));
+      setTimeout(() => {this.getSystemResponse(lastCommand)}, 100);
     }
   }
+
+  
+  sendSystemMessage(messageContent){
+    this.recentMessages.push(new MessageComponent(messageContent.split('\n'), false));
+    setTimeout(() => {this.scrollDownMessageContainer()}, 100);
+  }
+
 
   scrollDownMessageContainer() {
     this.messageContainer.nativeElement.scrollTop = this.messageContainer.nativeElement.scrollHeight;
   }
 
-  getHelp() {
-    const helpText = `
-    
-    `
-    this.recentMessages.push(new MessageComponent(helpText, false));
-    setTimeout(() => {this.scrollDownMessageContainer()}, 100);
+
+  getSystemResponse(command){
+    command = command.trim();
+    let commandName = command.split(' ')[0];
+
+    if (this.mainCommands.indexOf(command) > -1){
+      this.handleMainCommand(command);
+    }
+    if (this.additionalCommands.indexOf(commandName) > -1){
+      this.handleAdditionalCommand(command, commandName);
+    }
+    this.scrollDownMessageContainer();
   }
+
+
+  handleMainCommand(command){
+    // TODO: handling main commands
+    this.sendSystemMessage('<b>Okay!</b>');
+  }
+
+
+  handleAdditionalCommand(command, commandName){
+    if (commandName === 'weather'){
+        if (!this.checkArgs(command, 1)) return;
+        let city = command.split(' ')[1];
+        this.restService.getWeatherData(city).subscribe(data => {
+          let weatherInfo = 'Now in ' + city + ' <b>' + data['main']['temp'] + '&#176;C</b> and ' + data['weather'][0]['description'];
+          this.sendSystemMessage(weatherInfo);
+        }
+      );
+    }
+    if (commandName === 'help'){
+      if (!this.checkArgs(command, 0)) return;
+      this.getHelp();
+    }
+    // TODO: handling
+    if (commandName === 'open'){
+
+    }
+  }
+
+
+  checkArgs(commandName, needed){
+    let argsNum = commandName.split(' ').length - 1
+    if (argsNum !== needed){
+      let systemMessage = 'You have <b>' + argsNum.toString() + '</b> arguments, but <b>' + needed.toString() + '</b> needed!';
+      this.sendSystemMessage(systemMessage);
+      return false;
+    }
+    return true;
+  }
+
+  getHelp() {
+    let helpText = '<i>Help</i>';
+    helpText += '\n<b>Main commands:</b>'
+    this.mainCommands.forEach((commandName) => {
+      helpText += '\n-' + commandName;
+    });
+    helpText += '\n<b>Additional commands:</b>'
+    this.additionalCommands.forEach((commandName) => {
+      helpText += '\n-' + commandName;
+    });
+    this.sendSystemMessage(helpText);
+  }
+}
+
+
+if(typeof(String.prototype.trim) === "undefined")
+{
+    String.prototype.trim = function() 
+    {
+        return String(this).replace(/^\s+|\s+$/g, '');
+    };
 }
--- adviseMe/src/app/components/message/message.component.ts
+++ adviseMe/src/app/components/message/message.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
   
-  content: string;
+  content: string[];
   isYour: boolean;
 
-  constructor( content: string, isYour: boolean ) {
+  constructor( content: string[], isYour: boolean ) {
     this.content = content;
     this.isYour = isYour;
   }
